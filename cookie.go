2fb33c2fcb88e0b9e8d7c03dc5829cb3aae635b8
thisisaaronland <devnull@localhost>
2019-07-02 13:36:10 -0700

    initial commit

---
+++ cookie.go
@@ -0,0 +1,11 @@
+package cookie
+
+import (
+	go_http "net/http"
+)
+
+type Cookie interface {
+	Get(*go_http.Request) (string, error)
+	Set(go_http.ResponseWriter, string) error
+	Delete(go_http.ResponseWriter) error
+}
